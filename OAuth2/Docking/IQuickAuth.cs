380c425e68d33462111911017cfa4d4902d86d11
sloth4413 <512766594@qq.com>
2014-09-10 21:04:44 +0800

    添加第三方接入框架

---
+++ OAuth2/Docking/IQuickAuth.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OAuth2.Docking
+{
+    /// <summary>
+    /// 快捷授权登录
+    /// </summary>
+    public interface IQuickAuth
+    {
+        IUserAuth UserAuth { get; }
+
+        /// <summary>
+        /// 以唯一标识快捷登录
+        /// </summary>
+        /// <param name="openId">用户在第三方网站上的唯一标识</param>
+        /// <returns>返回true表示快捷登录成功,否则不成功/没有绑定</returns>
+        bool Login(string openId);
+    }
+}
---
+++ OAuth2/Docking/IUserAuth.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OAuth2.Docking
+{
+    /// <summary>
+    /// 用户登录授权
+    /// </summary>
+    public interface IUserAuth
+    {
+        /// <summary>
+        /// 以用户名和密码登录
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="userPassword"></param>
+        /// <returns>返回true表示登录成功,返回false表示不成功</returns>
+        bool Login(string userName, string userPassword);
+    }
+}
