a8060ebf3e43af02e9736d6cc5b5c39cadc82dd4
nbboy <512766594@qq.com>
2014-12-12 08:44:13 +0800

    clean

--- OAuth2.Example.Test/AliRequest.aspx.cs
+++ OAuth2.Example.Test/AliRequest.aspx.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using AliPay.Authorization;
 
 namespace OAuth2.Example.Test
